7ef5b791ac988ec13a0c9837bad2eb792b6d3d8e
Zachary Cush <zachcushonline@gmail.com>
2021-05-06 22:21:35 -0700

    Position pokeball clickables

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -37,32 +37,76 @@ const Message = styled.div`
   border-radius: 15px;
 `;
 
+const PokeButton1 = styled.div`
+  position: absolute;
+  width: 2.5vw;
+  height: 3.5vh;
+  margin-top: 6.5vh;
+  margin-right: 76vw;
+`;
+
+const PokeButton2 = styled.div`
+  position: absolute;
+  width: 2.5vw;
+  height: 3.5vh;
+  margin-top: 21.3vh;
+  margin-right: 56.3vw;
+`;
+
+const PokeButton3 = styled.div`
+  position: absolute;
+  width: 2.5vw;
+  height: 3.5vh;
+  margin-top: 21.3vh;
+  margin-right: 49.7vw;
+`;
+
 const PokeButton4 = styled.div`
   position: absolute;
-  background-color: red;
-  width: 2vw;
-  height: 3vh;
-  margin-top: 21.4vh;
+  width: 2.5vw;
+  height: 3.5vh;
+  margin-top: 21.3vh;
   margin-right: 43.1vw;
 `;
 
 const App = () => {
-  const { show, RenderModal } = useModal();
+  const { show: showA, RenderModal: RenderModalA } = useModal();
+  const { show: showB, RenderModal: RenderModalB } = useModal();
+  const { show: showC, RenderModal: RenderModalC } = useModal();
+  const { show: showD, RenderModal: RenderModalD } = useModal();
 
   return (
     <MainWrapper>
       <Background src={bgimg} />
       <Greeting>
-        Hello Gather Town Team! My name is Zach and I am a passionate Frontend Engineer! 
-        Here is an interactive resume that I've built. 
-        Click on the Pokeballs to learn more about me!
+        Hello Gather Town Team! My name is Zach and I am a passionate Frontend Engineer. 
+        Here is an interactive resume that I've built in the spirit of Gather Town! 
+        Click on the Pokeballs to learn more about me.
       </Greeting>
-      <PokeButton4 onClick={show}></PokeButton4>
-      <RenderModal>
+      <PokeButton1 onClick={showA}></PokeButton1>
+      <PokeButton2 onClick={showB}></PokeButton2>
+      <PokeButton3 onClick={showC}></PokeButton3>
+      <PokeButton4 onClick={showD}></PokeButton4>
+      <RenderModalA>
         <Message>
-          <p>blah blah</p>
+          <p>I am A</p>
         </Message>
-      </RenderModal>
+      </RenderModalA>
+      <RenderModalB>
+        <Message>
+          <p>I am B</p>
+        </Message>
+      </RenderModalB>
+      <RenderModalC>
+        <Message>
+          <p>I am C</p>
+        </Message>
+      </RenderModalC>
+      <RenderModalD>
+        <Message>
+          <p>I am D</p>
+        </Message>
+      </RenderModalD>
       <div id='modal-root' />
     </MainWrapper>
   );
