b8f8f5e8685de09245bfc2e176196c9ceec98190
Zachary Cush <zachcushonline@gmail.com>
2021-05-10 21:09:43 -0700

    Relocate style sheet to public dir and update routes

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
 app.use(express.json());
 const PORT = 3000;
 
